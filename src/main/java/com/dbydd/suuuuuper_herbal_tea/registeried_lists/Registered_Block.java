d7ff1443a8cf5dc598bce346e2b1ba6517a9d85c
hqyzsy <52950297+hqyzsy@users.noreply.github.com>
2020-07-12 16:39:05 +0800

    20200712 16.37 for resource (tea_tree models and big_black_pot models)

--- src/main/java/com/dbydd/suuuuuper_herbal_tea/registeried_lists/Registered_Block.java
+++ src/main/java/com/dbydd/suuuuuper_herbal_tea/registeried_lists/Registered_Block.java
@@ -6,7 +6,7 @@ import com.dbydd.suuuuuper_herbal_tea.blocks.TeaTree;
 import net.minecraft.block.Block;
 
 public class Registered_Block {
-    public static final Block TEST_TREE = new TeaTree("test_tree");
+    public static final Block TEA_TREE = new TeaTree("tea_tree");
     public static final Block EARTH_STOVETOP = new Earth_Stovetop();//todo rewrite
     public static final Block BIG_BLACK_POT = new Big_Black_Pot();
 
