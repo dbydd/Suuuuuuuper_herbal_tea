7fa519a999e24dfb04476e573494d66d60c75f06
hqyzsy <52950297+hqyzsy@users.noreply.github.com>
2020-07-19 16:24:38 +0800

    20200719 16.24 for resource (trees models)

--- src/main/java/com/dbydd/suuuuuper_herbal_tea/registeried_lists/Registered_Blocks.java
+++ src/main/java/com/dbydd/suuuuuper_herbal_tea/registeried_lists/Registered_Blocks.java
@@ -8,6 +8,9 @@ import net.minecraftforge.common.ToolType;
 
 public class Registered_Blocks {
     public static final Block TEST_TREE = new TeaTree();
+
+
+
     public static final Block EARTH_STOVETOP = new Earth_Stovetop();
     public static final Block BIG_BLACK_POT = new Big_Black_Pot();
     public static final Block WORLD_TEA_TREE_LEAVE = new World_Tea_Tree_Leave(Block.Properties.create(Material.LEAVES).notSolid().harvestTool(ToolType.AXE).hardnessAndResistance(0.5f), "world_tea_tree_leave_not_glow");
