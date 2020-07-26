5f6b1979dbbc07b827f0c89234acb0b97d13d140
hqyzsy <52950297+hqyzsy@users.noreply.github.com>
2020-07-26 11:39:54 +0800

    20200726 11.39 for many things

--- src/main/java/com/dbydd/suuuuuper_herbal_tea/events/SittingHandler.java
+++ src/main/java/com/dbydd/suuuuuper_herbal_tea/events/SittingHandler.java
@@ -20,7 +20,7 @@ public class SittingHandler {
     public static void sit(PlayerInteractEvent.RightClickBlock event) {
         if (event.getSide() == LogicalSide.SERVER) {
             BlockPos pos = event.getPos();
-            if (event.getWorld().getBlockState(pos).getBlock() == Registered_Blocks.STONE_CHAIR) {
+            if (event.getWorld().getBlockState(pos).getBlock() == Registered_Blocks.CHAIR) {
                 event.setCanceled(true);
                 Vec3d vec = new Vec3d(pos.getX()+0.5, pos.getY() + 0.5, pos.getZ()+0.5);
 
@@ -41,7 +41,7 @@ public class SittingHandler {
     public static void stand(EntityMountEvent event) {
         if (event.isDismounting() && event.getEntityBeingMounted() instanceof ArmorStandEntity) {
             Entity entityBeingMounted = event.getEntityBeingMounted();
-            if (event.getWorldObj().getBlockState(entityBeingMounted.getPosition()).getBlock() == Registered_Blocks.STONE_CHAIR && entityBeingMounted.isInvisible()) {
+            if (event.getWorldObj().getBlockState(entityBeingMounted.getPosition()).getBlock() == Registered_Blocks.CHAIR && entityBeingMounted.isInvisible()) {
                 entityBeingMounted.remove();
             }
         }
