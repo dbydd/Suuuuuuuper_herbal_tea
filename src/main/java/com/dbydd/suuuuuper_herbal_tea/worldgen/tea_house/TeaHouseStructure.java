b0f8cf9d73f346b311a98dc0e54c8305927ba30a
dbydd <1992003927@qq.com>
2020-08-18 09:53:03 +0800

    fixxxxxxxxx

--- src/main/java/com/dbydd/suuuuuper_herbal_tea/worldgen/tea_house/TeaHouseStructure.java
+++ src/main/java/com/dbydd/suuuuuper_herbal_tea/worldgen/tea_house/TeaHouseStructure.java
@@ -2,19 +2,15 @@ package com.dbydd.suuuuuper_herbal_tea.worldgen.tea_house;
 
 import com.mojang.datafixers.Dynamic;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MutableBoundingBox;
-import net.minecraft.world.IWorld;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeManager;
-import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.Structure;
 import net.minecraft.world.gen.feature.structure.StructureStart;
 import net.minecraft.world.gen.feature.template.TemplateManager;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Random;
 import java.util.function.Function;
